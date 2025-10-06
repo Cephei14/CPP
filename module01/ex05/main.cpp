7640218664303cba0dd90a274d4849f43eb0e306
cephei14 <rafik.dhaibi@gmail.com>
2025-10-06 23:07:50 +0900

    end ex05

---
+++ module01/ex05/main.cpp
@@ -0,0 +1,14 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl karen;
+
+	karen.complain("DEBUG");
+	karen.complain("INFO");
+	karen.complain("ERROR");
+	karen.complain("WARNING");
+	karen.complain("NOTHING");
+	
+	return 0;
+}
